29ac4e0cdcfd9711387f07bb937f7112e210b26f
nadiiapatrusheva <nadia.patrusheva@gmail.com>
2023-11-12 22:02:24 +0200

    abc

---
+++ ui_venv/lib/python3.11/site-packages/pytest_docker/__init__.py
@@ -0,0 +1,20 @@
+from .plugin import (
+    docker_cleanup,
+    docker_compose_command,
+    docker_compose_file,
+    docker_compose_project_name,
+    docker_ip,
+    docker_setup,
+    docker_cleanup,
+    docker_services,
+)
+
+__all__ = [
+    "docker_cleanup",
+    "docker_compose_file",
+    "docker_compose_project_name",
+    "docker_ip",
+    "docker_setup",
+    "docker_cleanup",
+    "docker_services",
+]
